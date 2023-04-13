b2212637dfc4ec7df40ca7a635b4f6dea76a02af
Tiago Luis Souza Pantoja <tiagopantoja1998@gmail.com>
2023-04-13 13:22:48 -0300

    Adicionando arquivos principais do projeto

---
+++ src/index.test.js
@@ -0,0 +1,112 @@
+import { describe, test, expect, beforeEach, vi } from 'vitest'
+import { httpStore } from '$lib/index'
+import { get } from 'svelte/store'
+
+describe('httpStore', () => {
+  beforeEach(() => {
+    global.fetch = vi.fn()
+  })
+
+  test('initial state is loading', async () => {
+    fetch.mockResolvedValueOnce({
+      json: () => {}
+    })
+
+    const store = httpStore('/cart')
+    const value = get(store)
+
+    expect(value).toEqual({ loading: true })
+    expect(fetch).toBeCalledWith('/cart', {})
+  })
+
+  test('sets state when fetch completes', async () => {
+    fetch.mockResolvedValueOnce({
+      json: async () => ({ someValue: 41 })
+    })
+
+    const store = httpStore('/cart')
+    get(store)
+
+    await flushPromises()
+
+    const value = get(store)
+
+    expect(value).toEqual({
+      loading: false,
+      someValue: 41
+    })
+
+    expect(fetch).toBeCalledWith('/cart', {})
+  })
+
+  test('refresh', async () => {
+    fetch
+      .mockResolvedValueOnce({
+        json: async () => ({ someValue: 41 })
+      })
+      .mockResolvedValueOnce({
+        json: async () => ({ someValue: 42 })
+      })
+
+    const store = httpStore('/cart')
+    const values = []
+
+    store.subscribe((value) => values.push(value))
+
+    await store.refresh()
+
+    expect(values).toEqual([
+      { loading: true },
+      { loading: true },
+      {
+        loading: false,
+        someValue: 41
+      },
+      {
+        loading: false,
+        someValue: 42
+      }
+    ])
+
+    expect(fetch).toHaveBeenCalledTimes(2)
+    expect(fetch).toBeCalledWith('/cart', {})
+  })
+
+  test('fetch', async () => {
+    fetch
+      .mockResolvedValueOnce({
+        json: async () => ({ someValue: 41 })
+      })
+      .mockResolvedValueOnce({
+        json: async () => ({ someValue: 42 })
+      })
+
+    const store = httpStore('/cart')
+    const values = []
+
+    store.subscribe((value) => values.push(value))
+
+    await store.fetch('/cart', { method: 'PATCH' })
+
+    expect(values).toEqual([
+      { loading: true },
+      { loading: true },
+      {
+        loading: false,
+        someValue: 41
+      },
+      {
+        loading: false,
+        someValue: 42
+      }
+    ])
+
+    expect(fetch).toHaveBeenCalledTimes(2)
+    expect(fetch).toBeCalledWith('/cart', {})
+    expect(fetch).toBeCalledWith('/cart', { method: 'PATCH' })
+  })
+})
+
+function flushPromises() {
+  return new Promise((resolve) => setImmediate(resolve))
+}
---
+++ src/lib/index.js
@@ -0,0 +1,29 @@
+import { readable, get } from 'svelte/store'
+
+export function httpStore(url, options = {}) {
+  let set
+  let data
+
+  const initialValue = { loading: true }
+  const store = readable(initialValue, (setter) => {
+    set = setter
+  })
+
+  get(store)
+
+  store.fetch = async (url, options = {}) => {
+    set({ loading: true, ...data })
+
+    data = await fetch(url, options).then((r) => r.json())
+
+    set({ loading: false, ...data })
+  }
+
+  store.refresh = () => {
+    return store.fetch(url, options)
+  }
+
+  store.refresh()
+
+  return store
+}
